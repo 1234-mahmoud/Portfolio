6dae188c155cb108fea5fae43fa0d17866847aa8
mahmoud elbalhi <mahmoud01234elbalhi@gmail.com>
2025-07-19 12:57:59 +0300

    up

--- data/data.js
+++ data/data.js
@@ -2,43 +2,61 @@
 const data = [
   {
     img: '/aprtio.jpg',
-    title: "Modern Homes",
+    title: `Modern Homes`,
     description:
-      "this is a simple landing page for web site that shows the art of the modern homes",
-    sourseCode: "https://github.com/1234-mahmoud/APRTIO",
-    liveDemo: "https://1234-mahmoud.github.io/APRTIO/",
+      `this is a simple landing page for web site that shows the art of the modern homes`,
+    sourseCode: `https://github.com/1234-mahmoud/APRTIO`,
+    liveDemo: `https://1234-mahmoud.github.io/APRTIO/`,
   },
   {
     img: '/landing.jpg',
-    title: "Simple Landing Page",
+    title: `Simple Landing Page`,
     description:
-      "this is a simple landing page for a business company that shows the stages of the company development",
-    sourseCode: "https://github.com/1234-mahmoud/landing_page",
-    liveDemo: "https://1234-mahmoud.github.io/landing_page/",
+      `this is a simple landing page for a business company that shows the stages of the company development`,
+    sourseCode: `https://github.com/1234-mahmoud/landing_page`,
+    liveDemo: `https://1234-mahmoud.github.io/landing_page/`,
   },
   {
     img: '/hyper.jpeg',
-    title: "Hyper Market",
+    title: `Hyper Market`,
     description:
-      "this a simple project of online store that contains some brances and different products",
-    sourseCode: "https://github.com/1234-mahmoud/Hyper_Market",
-    liveDemo: "https://1234-mahmoud.github.io/Hyper_Market/",
+      `this a simple project of online store that contains some brances and different products`,
+    sourseCode: `https://github.com/1234-mahmoud/Hyper_Market`,
+    liveDemo: `https://1234-mahmoud.github.io/Hyper_Market/`,
   },
   {
     img: '/kindle.jpg',
-    title: "Online Book Store",
+    title: `Online Book Store`,
     description:
-      "this a simple project of online books store that contains multiple books of Technology",
-    sourseCode: "https://github.com/1234-mahmoud/online_bookstore",
-    liveDemo: "https://1234-mahmoud.github.io/online_bookstore/",
+      `this a simple project of online books store that contains multiple books of Technology`,
+    sourseCode: `https://github.com/1234-mahmoud/online_bookstore`,
+    liveDemo: `https://1234-mahmoud.github.io/online_bookstore/`,
   },
   {
     img: '/food.jpeg',
-    title: "food store",
+    title: `food store`,
     description:
-      "this a simple landing page of online store of food that contains different food colors and drinks",
-    sourseCode: "https://github.com/1234-mahmoud/Food-Store",
-    liveDemo: "https://1234-mahmoud.github.io/Food-Store/",
+      `this a simple landing page of online store of food that contains different food colors and drinks`,
+    sourseCode: `https://github.com/1234-mahmoud/Food-Store`,
+    liveDemo: `https://1234-mahmoud.github.io/Food-Store/`,
+  },
+   {
+    img: '/venvl.jpeg',
+    title: `Venvl`,
+    description:
+      `VENVL is a rental platform for discovering premium daily and monthly properties..
+`,
+    sourseCode: `https://github.com/1234-mahmoud/Venvl`,
+    liveDemo: `https://venvl.vercel.app/`,
+  },
+     {
+    img: '/arx.jpeg',
+    title: `ARX`,
+    description:
+      `ARX Design is a real estate platform showcasing ARX Developments’ projects and services.
+`,
+    sourseCode: `https://github.com/1234-mahmoud/arx-design`,
+    liveDemo: `https://1234-mahmoud.github.io/arx-design/`,
   },
 ];
 
